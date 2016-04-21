26b0e97b9c336a2f90ced5d169aeda583b143ac4
Michael Baltic <michaelbaltic@MacBook-Pro.local>
2016-04-21 13:14:46 -0400

    dashboard mock

--- js/dashboard.js
+++ js/dashboard.js
@@ -22,50 +22,19 @@ $(document).ready(function () {
 
 	}, 1300);
 
-	var lineData = {
-		labels: ["January", "February", "March", "April", "May", "June", "July"],
-		datasets: [
-			{
-				label: "Example dataset",
-				fillColor: "rgba(220,220,220,0.5)",
-				strokeColor: "rgba(220,220,220,1)",
-				pointColor: "rgba(220,220,220,1)",
-				pointStrokeColor: "#fff",
-				pointHighlightFill: "#fff",
-				pointHighlightStroke: "rgba(220,220,220,1)",
-				data: [65, 59, 80, 81, 56, 55, 40]
-			},
-			{
-				label: "Example dataset",
-				fillColor: "rgba(26,179,148,0.5)",
-				strokeColor: "rgba(26,179,148,0.7)",
-				pointColor: "rgba(26,179,148,1)",
-				pointStrokeColor: "#fff",
-				pointHighlightFill: "#fff",
-				pointHighlightStroke: "rgba(26,179,148,1)",
-				data: [28, 48, 40, 19, 86, 27, 90]
-			}
-		]
-	};
+	var barChartData = {};
 
-	var lineOptions = {
-		scaleShowGridLines: true,
-		scaleGridLineColor: "rgba(0,0,0,.05)",
-		scaleGridLineWidth: 1,
-		bezierCurve: true,
-		bezierCurveTension: 0.4,
-		pointDot: true,
-		pointDotRadius: 4,
-		pointDotStrokeWidth: 1,
-		pointHitDetectionRadius: 20,
-		datasetStroke: true,
-		datasetStrokeWidth: 2,
-		datasetFill: true,
-		responsive: true
-	};
+	barChartData.labels = _labels;
 
+	barChartData.datasets = _.map(_datasets, function (_dataset) {
+		var type = _dataset.type;
+		if (!_datasetOptions.hasOwnProperty(type)) {
+			type = 'default';
+		}
+		return _.extend({}, _dataset, _datasetOptions[type]);
+	});
 
-	var ctx = document.getElementById("lineChart").getContext("2d");
-	var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
+	var _barChart = document.getElementById('barChart').getContext("2d");
+	var myBarChart = new Chart(_barChart).Bar(barChartData, _options);
 
 });
